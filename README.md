e556d4cfcc72872a26cca6505dd0a922e9f4d970
Blakthorn <blakthorn@hotmail.com>
2012-08-10 21:22:08 -0400

    Creating Project.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project I created to learn Java, nothing special here.
---
+++ src/com/blakthorn/mediaplayer/mediaplayer.java
@@ -0,0 +1,5 @@
+public class MediaPlayer{
+  public static void main(String[] args) {
+	System.out.println("Hello World!");
+  }
+}
